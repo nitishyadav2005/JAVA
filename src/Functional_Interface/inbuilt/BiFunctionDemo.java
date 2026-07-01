ec1cf0db300fa036a4325cfaad7c25ceb40a0312
Nitish Yadav <nitishyadav1817@gmail.com>
2026-07-01 09:08:13 +0530

    bifunction

--- src/Functional_Interface/inbuilt/BiFunctionDemo.java
+++ src/Functional_Interface/inbuilt/BiFunctionDemo.java
@@ -1,6 +1,7 @@
 package Functional_Interface.inbuilt;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class BiFunctionDemo {
 
@@ -10,6 +11,11 @@ public class BiFunctionDemo {
     static void main(String[] args) {
         System.out.println(addFunction.apply(10,20));
         System.out.println(subFunction.apply(20,30));
+
+        Function<Integer, Integer> multiplyBy2 = (a) -> a*2;
+        BiFunction<Integer, Integer, Integer> combinedFunction = addFunction.andThen(multiplyBy2);
+
+        System.out.println(combinedFunction.apply(2,5));
 
     }
 }
